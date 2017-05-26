c48a179f914c2a8050d882b9b77ecb3282122bd4
Eiji Sakai <eijisakai@gmail.com>
2017-05-26 17:07:57 +0900

    Fix so that the app can run on Heroku

--- app.py
+++ app.py
@@ -11,21 +11,27 @@ from lib import nn
 app = Flask(__name__)
 predictor = None
 
+def initialize():
+    app.config['MAX_CONTENT_LENGTH'] = 10 * 1024 * 1024 # 10MiB
+    global predictor
+    if not predictor:
+        predictor = nn.Predictor()
+
 @app.route('/')
 def index():
+    initialize()
     return render_template('index.html', items=predictor.get_sample_items())
 
 @app.route('/upload', methods=['POST'])
 def upload():
+    initialize()
     upfile = request.files["upfile"]
     img_file = "image" + str(random.randint(0, 999999)) + os.path.splitext(upfile.filename)[1]
-    img_path = os.path.join("./tmp/", img_file)
+    img_path = os.path.join("./cache/", img_file)
     upfile.save(img_path)
     item = predictor.get_image_item(img_path)
     os.remove(img_path)
     return json.dumps(item)
 
 if __name__ == '__main__':
-    app.config['MAX_CONTENT_LENGTH'] = 10 * 1024 * 1024 # 10MiB
-    predictor = nn.Predictor()
     app.run()
